2a0f8306ee76ae4371159ca0cb42dac6f45a8b4e
neo-g-27 <neo-g-27@icloud.com>
2025-12-28 17:12:48 +0900

    Lintチェック修正

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,6 @@ end
 Rails.application.routes.draw do
   root "home#top"
   resources :users, only: %i[new create]
-  get 'login', to: 'user_sessions#new', as: :login
-  post 'login', to: 'user_sessions#create'
+  get "login", to: "user_sessions#new", as: :login
+  post "login", to: "user_sessions#create"
 end
