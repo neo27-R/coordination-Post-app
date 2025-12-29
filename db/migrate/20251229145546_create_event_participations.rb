79b898fa938fa01f87a6d456d6e28bc72e3e8aa5
neo-g-27 <neo-g-27@icloud.com>
2025-12-30 01:53:25 +0900

    Lintチェック修正

--- db/migrate/20251229145546_create_event_participations.rb
+++ db/migrate/20251229145546_create_event_participations.rb
@@ -1,6 +1,6 @@
 class CreateEventParticipations < ActiveRecord::Migration[7.2]
-  add_index :event_participations, [:user_id, :event_id], unique: true
-  
+  add_index :event_participations, [ :user_id, :event_id ], unique: true
+
   def change
     create_table :event_participations do |t|
       t.references :user, null: false, foreign_key: true
