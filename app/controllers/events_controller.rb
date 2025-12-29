c099cb24bfddb719d8d8baf8e76d943133e9c718
neo-g-27 <neo-g-27@icloud.com>
2025-12-29 22:51:07 +0900

    add:イベント詳細・編集・削除機能の追加

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,26 +1,55 @@
 class EventsController < ApplicationController
   before_action :require_login
+  before_action :set_event, only: %i[show edit update destroy]
+  before_action :authorize_event!, only: %i[edit update destroy]
 
   def index
     @events = Event.order(created_at: :desc)
   end
 
+  def show
+  end
+
   def new
-    @event = Event.new
+    @event = current_user.events.build
   end
 
   def create
-    @event = Event.new(event_params)
-    @event.user = current_user
+    @event = current_user.events.build(event_params)
 
     if @event.save
-      redirect_to events_path, notice: "イベントを作成しました"
+      redirect_to @event, notice: "イベントを作成しました"
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @event.update(event_params)
+      redirect_to @event, notice: "イベントを更新しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @event.destroy
+    redirect_to events_path, notice: "イベントを削除しました"
+  end
+
   private
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def authorize_event!
+    return if @event.user_id == current_user.id
+    redirect_to @event, alert: "権限がありません"
+  end
 
   def event_params
     params.require(:event).permit(:title, :body, :start_at)
