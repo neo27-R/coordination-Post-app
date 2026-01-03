e2f7699cc430e4e7e2d1bef2a26727c8d3c0a5ab
neo-g-27 <neo-g-27@icloud.com>
2026-01-03 14:25:25 +0900

    add:フラッシュメッセージの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,17 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+  
     if @user.save
       auto_login(@user)
       redirect_to events_path, notice: "登録しました"
     else
+      if @user.errors.added?(:password_confirmation, :confirmation)
+        flash.now[:alert] = "パスワード（確認）が一致しません"
+      else
+        flash.now[:alert] = @user.errors.full_messages.first || "入力内容を確認してください"
+      end
+  
       render :new, status: :unprocessable_entity
     end
   end
