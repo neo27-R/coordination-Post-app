8cd4b6d46ad1444c0de172f167881f632439e2ba
neo-g-27 <neo-g-27@icloud.com>
2026-01-03 12:56:24 +0900

    fix:render_error

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,6 +1,10 @@
 set -o errexit
+set -o xtrace
 
+echo "=== RENDER BUILD SCRIPT START ==="
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+echo "=== RUN MIGRATIONS ==="
 bundle exec rails db:migrate
+echo "=== MIGRATIONS DONE ==="
